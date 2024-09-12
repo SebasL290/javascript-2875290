fad00fccd53eadc2a3e03c013cd224d8c89237c2
SebasL290 <jsebastianlopez.0906@gmail.com>
2024-09-12 15:44:32 -0500

    FINAL

--- 10-aiden-app/js/app.js
+++ 10-aiden-app/js/app.js
@@ -1,22 +1,103 @@
-const sectionDesafios = document.querySelector('.desafios');
+//abrir popup
+const wrapper = document.querySelector('.wrapper');
+const inicolink = document.querySelector('.inico-link');
+const registrarselink = document.querySelector('.registrarse-link');
+const btnpopup = document.querySelector('.btninicio-popup');
+const iconclose = document.querySelector('.icon-close');
+const logoutBtn = document.getElementById('btncerrars');
+const saludo = document.getElementById('saludo')
+const usuarioinput = document.getElementById('usuario')
+const contraseñainput = document.getElementById('contraseña')
+const recordarmeCheckbox = document.getElementById('recordarme')
 
 
-function mostrarResults(Aiden) {
-    Aiden.forEach(element => {
-        const desafio = document.createElement('div');
-        desafio.classList.add("desafios-container");
-        desafio.innerHTML = `
-    <div class="desafio1">
-        <h1>${element.desafio}</h1>
-        <h2>${element.textodescripcion}</h2>
-        <button>
-        <a href="${element.comenzar}">Comenzar</a>
-        </button>
-    </div>
-    `;
-        container.appendChild(desafio);
-    });
-    mostrarResults(Aiden);
+
+registrarselink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+inicolink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnpopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+ 
+iconclose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+//registro
+const formulario = document.querySelector('.registro')
+const  ingresar = document.querySelector('.inicio')
+const username = document.querySelector('.usernamer')
+const password = document.querySelector('.passwordr')
+const email = document.querySelector('#correo')
+
+function registrarUser(e) {
+    e.preventDefault();
+    let user = {
+        userName: username.value.toLowerCase(),
+        userPass: password.value.toLowerCase(),
+        userEmail: email.value.toLowerCase(),
+        
+
+    }
+    localStorage.setItem('user', JSON.stringify(user))
+    /* formulario.reset();  */
 }
+formulario.addEventListener('submit', registrarUser)
+let robo =JSON.parse(localStorage.getItem("user"))
+
+
+//inicio de sesion 
+function validarUsuario (e){
+e.preventDefault()
+let User = JSON.parse( localStorage.getItem('user'))
+console.log(User.userName)
+console.log(User.userPass)
+
+console.log(Boolean(username.value === User.userName))
+console.log(Boolean(password.value === User.userPass))
+
+    if(username.value === User.userName && password.value === User.userPass){
+        console.log('felicidades puedes entrar ')
+        window.location = './index.html' 
+    }else{
+        console.log('sigue intentando')
+    }
+   /*  formulario.reset()   */
+}
+ingresar.addEventListener('submit',validarUsuario)
+
+ 
+
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const usuario = usuarioinput.value;
+    const contraseña = contraseñainput.value;
+
+    if (recordarmeCheckbox.checked) {
+        localStorage.setItem('usuario', usuario);
+        localStorage.setItem('contraseña', contraseña)
+    }
+
+    saludo.innerHTML = `Hola, ${usuario}`;
+    logoutBtn.style.display = 'inline';
+    wrapper.classList.remove('active-popup');
+
+});
+
+
+btncerrars.addEventListener('click', () => {
+    localStorage.removeItem('usuario');
+    saludo.innerHTML = '';
+    btncerrars.style.display = 'none';
+});
+
+
+
 
 
--- 10-aiden-app/js/bd.js
+++ 10-aiden-app/js/bd.js
@@ -1,95 +1,37 @@
-let Aiden = [
-        {
-        id: 1,
-        imagenplaneta: "../sources/images/",
-        mision: 0,
-        textodescripcion: "texto",
-        comenzar: '../vistas/modulo1.html',
-        titulo: "modulodeintroduccion",
-        videos: ['',''],
-        desafios: [
+const desafios = [
             {
-            imagenplanta: "../sources/image/desafio1.png",
-            desafio: "DESAFÍO 1",
-            textodescripcion: " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenzar: './desafios.html',
+                id: 'desafio1',
+                titulo: 'DESAFÍO 1',
+                descripcion: `
+                    Durante el curso, hemos explorado<br>
+                    las herramientas, técnicas y conceptos<br>
+                    que transforman el mundo del arte digital.<br>
+                    ¿Están listos para demostrar tus conocimientos<br>
+                    en este campo apasionante?
+                `,
+                enlace: '../vistas/desafios.html'
             },
             {
-            imagenplanta: "../sources/image/desafio2.png",
-            desafio: "DESAFÍO 2",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'
+                id: 'desafio2',
+                titulo: 'DESAFÍO 2',
+                descripcion: `
+                    Durante el curso, hemos explorado<br>
+                    las herramientas, técnicas y conceptos<br>
+                    que transforman el mundo del arte digital.<br>
+                    ¿Están listos para demostrar tus conocimientos<br>
+                    en este campo apasionante?
+                `,
+                enlace: '../vistas/desafios.html'
             },
             {
-            imagenplanta: "../sources/image/desafio3.png",
-            desafio: "DESAFÍO 3",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'   
-            }
-        ]
-    },
-        {
-        id: 2,
-        imagenplaneta: "../sources/images/",
-        mision: 1,
-        textodescripcion: "texto",
-        comenzar: '../vistas/modulo2.html',
-        titulo: "modulodefotografia",
-        videos: ['',''],
-        desafios: [
-            {
-            imagenplanta: "../sources/image/desafio1.png",
-            desafio: "DESAFÍO 1",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'
-            },
-            {
-            imagenplanta: "../sources/image/desafio2.png",
-            desafio: "DESAFÍO 2",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'
-            },
-            {
-            imagenplanta: "../sources/image/desafio3.png",
-            desafio: "DESAFÍO 3",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'   
-            }
-        ]
-    },
-        {
-        id: 3,
-        imagenplaneta: "../sources/images/",
-        mision: 2,
-        textodescripcion: "texto",
-        comenazar: '../vistas/modulo3.html',
-        titulo: "modulodeilustracion",
-        videos: ['',''],
-        desafios: [
-            {
-            imagenplanta: "../sources/image/desafio1.png",
-            desafio: "DESAFÍO 1",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'
-            },
-            {
-            imagenplanta: "../sources/image/desafio2.png",
-            desafio:  "DESAFÍO 2",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'
-            },
-            {
-            imagenplanta: "../sources/image/desafio3.png",
-            desafio: "DESAFÍO 3",
-            textodescripcion:  " Durante el curso, hemos explorado<br>las herramientas, técnicas y conceptos<br>que transforman el mundodel arte digital.<br>¿Están listos para demostrar tus conocimientos <br>en este campo apasionante?",
-            comenazar: './desafios.html'   
-            }
-        ]
-    },
-]
-
-
-
-
-
-
+                id: 'desafio3',
+                titulo: 'DESAFÍO 3',
+                descripcion: `
+                    Durante el curso, hemos explorado<br>
+                    las herramientas, técnicas y conceptos<br>
+                    que transforman el mundo del arte digital.<br>
+                    ¿Están listos para demostrar tus conocimientos<br>
+                    en este campo apasionante?
+                `,
+                enlace: '../vistas/desafios.html'
+            }];
